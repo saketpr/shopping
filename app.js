fdc78c1e416239aaea40d9a065a10e0f2739dd14
insidescoope1 <krishany03@gmail.com>
2016-12-25 21:29:22 +0530

    Some code done

---
+++ app.js
@@ -0,0 +1,14 @@
+// piece of code that does one or more actions
+
+// function goal(name, salary){
+// 	alert("Hi "+ name);
+// 	alert("your salary is great "+ salary);
+// }
+
+// goal('Krishna', 200000);
+// goal('Nilesh',45000);
+
+function add(first, second){
+	return first+second;
+}
+alert(add(9,3));
